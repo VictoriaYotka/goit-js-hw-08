e162a2775c35695cee42980f2424f0b2c1501611
VictoriaYotka <victoria.borisyuk@gmail.com>
2023-03-11 14:51:21 +0200

    task-2 done

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,16 @@
 
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
+
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe);
+
+const TIME_KEY = 'videoplayer-current-time';
+
+const onTimeUpdate = (event) => localStorage.setItem(TIME_KEY, JSON.stringify(event.seconds));
+
+player.on('timeupdate', throttle(onTimeUpdate, 1000));
+
+const currentTime = JSON.parse(localStorage.getItem(TIME_KEY));
+
+player.setCurrentTime(currentTime);
