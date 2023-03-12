dc790cf70e7b570f3d3c94be5a3e8f1d1a6ce498
VictoriaYotka <victoria.borisyuk@gmail.com>
2023-03-12 20:58:22 +0200

    task-3 done

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,39 @@
+import throttle from 'lodash.throttle';
 
+const formEl = document.querySelector('.feedback-form');
+const inputEl = document.querySelector('input');
+const textAreaEl = document.querySelector('textarea');
+
+const USER_DATA = 'feedback-form-state';
+
+const savedData = JSON.parse(localStorage.getItem(USER_DATA));
+
+const handlerFormFieldsInput = () => {
+    const email = inputEl.value;
+    const message = textAreaEl.value;
+
+    const dataObj = { email, message };
+
+    localStorage.setItem(USER_DATA, JSON.stringify(dataObj));
+};
+
+const handlerFormSubmit = (event) => {
+    event.preventDefault();
+    localStorage.removeItem(USER_DATA);
+    event.currentTarget.reset();
+
+};
+
+const updateInputValues = () => {
+if(localStorage.getItem(USER_DATA)) {
+    inputEl.value = savedData.email;
+    textAreaEl.value = savedData.message;
+}
+
+return
+}
+
+updateInputValues();
+
+formEl.addEventListener('input', throttle(handlerFormFieldsInput, 500))
+formEl.addEventListener('submit', handlerFormSubmit)
